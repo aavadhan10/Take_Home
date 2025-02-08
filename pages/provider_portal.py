da71fc7fe3a527314d99741bb7b0112dea99ca97
aavadhan10 <166965425+aavadhan10@users.noreply.github.com>
2025-02-07 23:28:33 -0800

    Update provider_portal.py

--- pages/provider_portal.py
+++ pages/provider_portal.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Page Configuration
 st.set_page_config(
-    page_title="Moxie Provider Portal",
+    page_title="Provider Portal View",
     page_icon="🚀",
     layout="wide",
     initial_sidebar_state="collapsed"
